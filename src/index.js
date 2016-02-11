cea7e5ed58a0d6125a0713575bc15835d2e3ff7b
PAK90 <paklavins15@hotmail.com>
2016-02-10 23:42:56 -0700

    Added data and images. Tried replicating proper search page but more syntax errors.

--- src/index.js
+++ src/index.js
@@ -1,50 +1,129 @@
 import * as React from "react";
+import * as _ from "lodash";
 
-import Searchkit from "searchkit";
+import {
+  SearchBox,
+  Hits,
+  HitsStats,
+  RefinementListFilter,
+  Pagination,
+  ResetFilters,
+  MenuFilter,
+  SelectedFilters,
+  HierarchicalMenuFilter,
+  NumericRefinementListFilter,
+  SortingSelector,
+  SearchkitComponent,
+  SearchkitProvider,
+  SearchkitManager,
+  NoHits,
+  RangeFilter,
+  InitialLoader
+} from "searchkit";
 
-require('./style.scss');
+const MovieHitsItem = (props)=> {
+  const {bemBlocks, result} = props
+  let url = "http://www.imdb.com/title/" + result._source.imdbId
+  return (
+    <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
+      <a href={url} target="_blank">
+        <img data-qa="poster" className={bemBlocks.item("poster")} src={result._source.poster} width="170" height="240"/>
+      </a>
+      <a href={url} target="_blank">
+        <div data-qa="title" className={bemBlocks.item("title")} dangerouslySetInnerHTML={{__html:_.get(result,"highlight.title",false) || result._source.title}}>
+        </div>
+      </a>
+    </div>
+  )
+}
 
-const host = "https://kili-eu-west-1.searchly.com/movies/";
-const sk = new Searchkit.SearchkitManager(host, {
-    multipleSearchers:false,
-    basicAuth:"read:teetndhjnrspbzxxyfxmf5fb24suqxuj"
-});
 
+export class App extends React.Component<any, any> {
 
-class MovieHits extends Searchkit.Hits {
-    renderResult(result:any) {
-        let url = "http://www.imdb.com/title/" + result._source.imdbId;
-        return (
-            <div className={this.bemBlocks.item().mix(this.bemBlocks.container("item"))} key={result._id}>
-                <a href={url} target="_blank">
-                    <img className={this.bemBlocks.item("poster")} src={result._source.poster} width="100" height="140"/>
-                    <div className={this.bemBlocks.item("title")}>{result._source.title}</div>
-                </a>
+  searchkit:SearchkitManager
+
+  constructor() {
+    const host = "http://demo.searchkit.co/api/movies"
+    this.searchkit = new SearchkitManager(host)
+    this.searchkit.translateFunction = (key)=> {
+      return {"pagination.next":"Next Page", "pagination.previous":"Previous Page"}[key]
+    }
+    super()
+  }
+
+  render(){
+
+    return (
+      <div>
+      <SearchkitProvider searchkit={this.searchkit}>
+      <div>
+        <div className="layout">
+
+          <div className="layout__top-bar top-bar">
+            <div className="top-bar__content">
+              <div className="my-logo">Searchkit Acme co</div>
+              <SearchBox
+                translations={{"searchbox.placeholder":"search movies"}}
+                queryOptions={{"minimum_should_match":"70%"}}
+                autofocus={true}
+                searchOnChange={true}
+                queryFields={["actors^1","type^2","languages","title^5", "genres^2"]}/>
             </div>
-        );
-    }
-}
+          </div>
 
-class Application extends React.Component {
-    render() {
-        const SearchkitProvider = Searchkit.SearchkitProvider;
-        const Searchbox = Searchkit.SearchBox;
-        const Hits = Searchkit.Hits;
-        return (<div>
+          <div className="layout__body">
 
-            <SearchkitProvider searchkit={sk}>
-                <div className="search">
-                   <div className="search__query">
-                   <Searchbox searchOnChange={true} prefixQueryFields={["actors^1","type^2","languages","title^10"]} />
-                   </div>
-                   <div className="search__results">
-                   <MovieHits hitsPerPage={6}/>
-                   </div>
+                <div className="layout__filters">
+                    <HierarchicalMenuFilter fields={["type.raw", "genres.raw"]} title="Categories" id="categories"/>
+              <RangeFilter min={0} max={100} field="metaScore" id="metascore" title="Metascore" showHistogram={true}/>
+              <RangeFilter min={0} max={10} field="imdbRating" id="imdbRating" title="IMDB Rating" showHistogram={true}/>
+              <RefinementListFilter id="actors" title="Actors" field="actors.raw" size={10}/>
+                    <RefinementListFilter translations={{"facets.view_more":"View more writers"}} id="writers" title="Writers" field="writers.raw" operator="OR" size={10}/>
+                    <RefinementListFilter id="countries" title="Countries" field="countries.raw" operator="OR" size={10}/>
+              <NumericRefinementListFilter id="runtimeMinutes" title="Length" field="runtimeMinutes" options={[
+                {title:"All"},
+                {title:"up to 20", from:0, to:20},
+                {title:"21 to 60", from:21, to:60},
+                {title:"60 or more", from:61, to:1000}
+              ]}/>
+            </div>
+
+                <div className="layout__results results-list">
+
+              <div className="results-list__action-bar action-bar">
+
+                <div className="action-bar__info">
+                        <HitsStats translations={{
+                    "hitstats.results_found":"{hitCount} results found"
+                  }}/>
+                        <SortingSelector options={[
+                            {label:"Relevance", field:"_score", order:"desc",defaultOption:true},
+                            {label:"Latest Releases", field:"released", order:"desc"},
+                            {label:"Earliest Releases", field:"released", order:"asc"}
+                        ]}/>
+                    </div>
+
+                <div className="action-bar__filters">
+                  <SelectedFilters/>
+                  <ResetFilters/>
                 </div>
-            </SearchkitProvider>
 
-        </div>);
-    }
+              </div>
+                    <Hits hitsPerPage={12} highlightFields={["title"]}
+                    itemComponent={MovieHitsItem} sourceFilter={["title", "poster", "imdbId"]}
+                    scrollTo="body"
+              />
+              <NoHits suggestionsField={"title"}/>
+              <InitialLoader/>
+                    <Pagination showNumbers={true}/>
+                </div>
+          </div>
+                <a className="view-src-link" href="https://github.com/searchkit/searchkit-demo/blob/master/src/app/src/App.tsx">View source »</a>
+            </div>
+      </div>
+      </SearchkitProvider>
+      </div>
+    )}
 }
 
-React.render(<Application />, document.getElementById('app'));
+ReactDOM.render(<Application />, document.getElementById('app'));
