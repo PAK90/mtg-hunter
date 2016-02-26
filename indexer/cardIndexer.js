03966c8b17552c76cf03afe782ab83c8f7e74026
PAK90 <paklavins15@hotmail.com>
2016-02-25 19:00:23 -0700

    Made indexer upload 1000 at a time, hopefully reducing memory footprint.

--- indexer/cardIndexer.js
+++ indexer/cardIndexer.js
@@ -2,6 +2,7 @@
 let cards = require("./data/allCardsWithSetsExt.js");
 let indexer = require("./indexer");
 let _ = require("lodash");
+
 function symbolize(manaCost) {
   var symbolArray = [];
   var regEx = /{([^}(0-9)]+)}/g; // Match all curly braces without numbers in them.
@@ -24,16 +25,6 @@ function countColours(symbols) {
   }
   return count;
 }
-let cardDocs = _.map(cards, (card)=> {
-	card.codes = _.map(card.multiverseids, "setCode");
-  card.formats = _.map(card.legalities, "format");
-  card.codeNames = _.map(card.multiverseids, "setName");
-  card.colourCount = card.colors ? card.colors.length : 0; // If it doesn't have colours it won't exist so hopefully it's false-y and will go to 0.
-  card.colors = card.colors || "Colourless";
-  card.symbols = _.uniq(symbolize(card.manaCost)); // Extract all symbols from {} that aren't numeric. Remove duplicates with _.uniq.
-  //card.colourCount = countColours(card.symbols); // Count unique colours. ['w','ug'] = 3, ['r','u','w'] = 3, ['c'] = 0 since colourless isn't a colour. Replaced by line 30.
-	return card;
-});
 
 let stringWithRaw = {
   type:"string",
@@ -70,7 +61,31 @@ let cardIndexer = new indexer(
   "cards", "card"
 );
 
+let cardLength = cards.length;
+var i = 0;
+
+function bulkLoop() {
+  setTimeout(function() {
+    console.log(i + ' to ' + (i + 999) + ' (' + cards.slice(i,i+1000).length + ')');
+    let cardDocs = _.map(cards.slice(i, i+1000), (card)=> {
+      card.codes = _.map(card.multiverseids, "setCode");
+      card.formats = _.map(card.legalities, "format");
+      card.codeNames = _.map(card.multiverseids, "setName");
+      card.colourCount = card.colors ? card.colors.length : 0; // If it doesn't have colours it won't exist so hopefully it's false-y and will go to 0.
+      card.colors = card.colors || "Colourless";
+      card.symbols = _.uniq(symbolize(card.manaCost)); // Extract all symbols from {} that aren't numeric. Remove duplicates with _.uniq.
+      //card.colourCount = countColours(card.symbols); // Count unique colours. ['w','ug'] = 3, ['r','u','w'] = 3, ['c'] = 0 since colourless isn't a colour. Replaced by line 30.
+      return card;
+    });
+    i += 1000;
+    cardIndexer.bulkInsertDocuments(cardDocs);
+    if (i < cardLength) {
+      bulkLoop();
+    }
+  }, 5000);
+}
+
 cardIndexer.setMapping(mapping);
 cardIndexer.createMappingAndIndex().then(()=> {
-	cardIndexer.bulkInsertDocuments(cardDocs);
+  bulkLoop();
 });
