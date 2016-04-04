5a351e54450c1a5f823fc30de60af753c7815fed
Gregory Potdevin <gregory.potdevin@gmail.com>
2016-04-04 23:52:35 +0200

    fix hw in MultiSelect

--- src/CostSymbols.js
+++ src/CostSymbols.js
@@ -7,7 +7,7 @@ export default class CostSymbols extends React.Component {
     if (!cost) return null
     return (
       <span>
-        {cost.toLowerCase().match(/([0-z]\/[0-z])|[0-z,½,∞]/g).map(function (basename, i) {
+        {cost.toLowerCase().match(/([0-z]\/[0-z])|hw|[0-z,½,∞]/g).map(function (basename, i) {
           var src = './src/img/' + basename.replace("/", "").toLowerCase() + '.png';
             return <img key={i} src={src} height='15px' style={{marginBottom: -2}}/>;
         })}
