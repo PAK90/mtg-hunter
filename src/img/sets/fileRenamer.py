5220ab6efdc1d4a7201b3d21ad857965eab42b9a
Pēteris Kļaviņš <klavins@netspace.net.au>
2016-04-13 00:02:40 +0000

    Clean up graphics.

--- src/img/sets/fileRenamer.py
+++
@@ -1,8 +0,0 @@
-import re,sys,subprocess,os,struct,math
-from os import listdir
-from os.path import isfile, join
-
-for filename in os.listdir('.'):
-	if filename.startswith(sys.argv[1]):
-		os.rename(filename, filename.replace(sys.argv[1], sys.argv[2]))
-		print('renaming ' + filename + ' to have ' + sys.argv[2])
