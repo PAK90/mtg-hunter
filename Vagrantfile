d0de25f6c3c814af6e29429625f9f88963803649
Pēteris Kļaviņš <klavins@netspace.net.au>
2017-04-24 00:32:13 +0100

    Roll back Ubuntu and Elasticsearch vagrant updates.

--- Vagrantfile
+++ Vagrantfile
@@ -1,10 +1,6 @@
 Vagrant.configure("2") do |config|
-  config.vm.box = "ubuntu/xenial64"
-  config.vm.box_url = "https://atlas.hashicorp.com/ubuntu/boxes/xenial64"
+  config.vm.box = "debian/contrib-jessie64"  
+  config.vm.box_url = "https://atlas.hashicorp.com/debian/boxes/contrib-jessie64"
   config.vm.provision :shell, :path => "bootstrap.sh"
   config.vm.network :forwarded_port, guest: 9200, host: 9200
-  config.vm.provider :virtualbox do |vb|
-    vb.customize ["modifyvm", :id, "--memory", "3072", "--cpus", "2"]
-  end
-
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,14 +4,13 @@
 sudo apt-get update
 
 # install java
-sudo apt-get install openjdk-8-jre-headless -y
+sudo apt-get install openjdk-7-jre-headless -y
 
 # install elasticsearch
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.3.0.deb
-sudo dpkg -i elasticsearch-5.3.0.deb
-sudo sed -i '55anetwork.host: 0.0.0.0' /etc/elasticsearch/elasticsearch.yml
+wget https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.2.0/elasticsearch-2.2.0.deb
+sudo dpkg -i elasticsearch-2.2.0.deb
+sudo sed -i '54anetwork.host: 0.0.0.0' /etc/elasticsearch/elasticsearch.yml
 sudo cat >> /etc/elasticsearch/elasticsearch.yml << ENDOFTEXT
-
 http.cors.enabled : true  
 http.cors.allow-origin : "*"
 http.cors.allow-methods : OPTIONS, HEAD, GET, POST, PUT, DELETE
@@ -20,3 +19,6 @@ ENDOFTEXT
 sudo systemctl enable elasticsearch.service
 sudo systemctl daemon-reload
 sudo systemctl restart elasticsearch.service
+
+# install head
+sudo /usr/share/elasticsearch/bin/plugin install mobz/elasticsearch-head
